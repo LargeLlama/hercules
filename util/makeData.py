0e23985b3c8e6928c3ef9745d4d73b7823a0be84
ahuang20stuy <ahuang20@stuy.edu>
2019-05-13 14:05:26 -0400

    lol

--- util/makeData.py
+++ util/makeData.py
@@ -5,10 +5,10 @@ c = db.cursor()
 #create users
 command = "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE,password_hash TEXT)"
 c.execute(command)
-#create contributions
+#create calendar
 command = "CREATE TABLE calender(user_id INTEGER, date TEXT,schedule_name TEXT)"
 c.execute(command)
-#create stories
+#create templates
 command = "CREATE TABLE templates(user_id INTEGER,name TEXT,task TEXT,start_time INTEGER,end_time INTEGER)"
 c.execute(command)
 db.commit()
