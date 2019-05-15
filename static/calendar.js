44daa729fdc3f225aedd5e1138f1f8edf7b232cb
huiminwu <huiminwu24@gmail.com>
2019-05-15 13:25:07 -0400

    Add date format

--- static/calendar.js
+++ static/calendar.js
@@ -1,7 +1,7 @@
 document.querySelector("table").addEventListener('click', function (e) {
     var cell = e.target.closest("td");
     if (cell != null) {
-        console.log(cell.innerHTML);
+        console.log(cell.id);
     }
 });
 
