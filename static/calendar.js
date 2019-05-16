8e49bcc5ac1549a4f7da3d4bac28d3762168c4f3
Hui Min Wu <hwu5@stuy.edu>
2019-05-16 18:58:48 +0000

    Revert "Add files for apache server"
    This reverts commit 2ae962d67e570f97df5c85f5338c183de2a6cfe1.

---
+++ static/calendar.js
@@ -0,0 +1,7 @@
+document.querySelector("table").addEventListener('click', function (e) {
+    var cell = e.target.closest("td");
+    if (cell != null) {
+        console.log(cell.id);
+    }
+});
+
