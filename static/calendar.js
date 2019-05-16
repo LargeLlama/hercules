2ae962d67e570f97df5c85f5338c183de2a6cfe1
Hui Min Wu <hwu5@stuy.edu>
2019-05-16 18:43:18 +0000

    Add files for apache server

--- static/calendar.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("table").addEventListener('click', function (e) {
-    var cell = e.target.closest("td");
-    if (cell != null) {
-        console.log(cell.id);
-    }
-});
-
