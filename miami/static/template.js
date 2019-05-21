2fce504af6f7ec32e4091dbce497adffa70d2b8a
ahuang20stuy <ahuang20@stuy.edu>
2019-05-21 13:25:21 -0400

    xd

--- miami/static/template.js
+++ miami/static/template.js
@@ -6,9 +6,9 @@ add.onclick = function() {
     var cell0 = row.insertCell(0);
     var cell1 = row.insertCell(1);
     var cell2 = row.insertCell(2);
-    cell0.innerHTML = "<input type='text' name='task' placeholder='task name'>";
-    cell1.innerHTML = "<input type='time' name='start'>";
-    cell2.innerHTML="<input type='time' name='end'>";
+    cell0.innerHTML = "<input type='text' name='task' placeholder='task name' style='text-align:center;'>";
+    cell1.innerHTML = "<input type='time' name='start' style='text-align:center;'>";
+    cell2.innerHTML="<input type='time' name='end' style='text-align:center;'>";
     num+=1;
     
 };
