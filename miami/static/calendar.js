e3d3e59b92cd3900754e3ad01b05f00206582313
huiminwu <huiminwu24@gmail.com>
2019-05-24 13:33:32 -0400

    Add accumulation of dates

--- miami/static/calendar.js
+++ miami/static/calendar.js
@@ -1,7 +1,9 @@
+var ids = [];
 document.querySelector("table").addEventListener('click', function (e) {
     var cell = e.target.closest("td");
     if (cell != null) {
         console.log(cell.id);
     }
+    ids.push(cell.id);
+    console.log(ids);
 });
-
