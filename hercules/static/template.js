7d2b21de0bacded6eca6ea9b986dadc783f5588f
Rubin Peci <rubinpeci@gmail.com>
2019-06-13 19:02:37 -0400

    Updated name

---
+++ hercules/static/template.js
@@ -0,0 +1,21 @@
+var addButton = document.getElementById("add");
+var table=document.getElementById("temp");
+var num = 2;
+add.onclick = function() {
+    var row=table.insertRow(num);
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    cell0.innerHTML = "<div class='remove'> </div> <input type='text' name='task[]' placeholder='task name' style='text-align:center;'>";
+    cell1.innerHTML = "<input type='time' name='start[]' style='text-align:center;'>";
+    cell2.innerHTML="<input type='time' name='end[]' style='text-align:center;'>";
+    num+=1;
+    $(".remove").on('click',function(e){
+	$(this).parent().parent().remove();
+	if (num >2){
+	    num -=1
+	}
+    });
+
+};
+
